36b5e624ffac15755d9cb0f651b7c75e1966e876
ducda26 <doananhduc26@gmail.com>
2023-10-20 00:42:26 +0700

    create items in each store

--- app.py
+++ app.py
@@ -23,6 +23,17 @@ def get_stores():
 @app.post("/store")
 def create_store():
     request_data = request.get_json()
-    new_store = {"name": request_data["name"], "item": []}
+    new_store = {"name": request_data["name"], "items": []}
     stores.append(new_store)
     return new_store, 201
+
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item
+    return {"message": "Store not found"}, 404
