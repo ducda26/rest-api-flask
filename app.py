31485e1b28436a241b01101173ae6fce567da448
ducda26 <doananhduc26@gmail.com>
2023-10-20 00:19:27 +0700

    su dung phuong thuc POST tao new store

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -14,6 +14,15 @@ stores = [
     }
 ]
 
+
 @app.get("/store")
 def get_stores():
     return {"stores": stores}
+
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "item": []}
+    stores.append(new_store)
+    return new_store, 201
