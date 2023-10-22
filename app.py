243ea891c5980f24a49170f50608921a1b48091d
ducda26 <doananhduc26@gmail.com>
2023-10-23 02:56:55 +0700

    Improvements to our first REST API

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
+from flask_smorest import abort
 from db import stores, items
 import uuid
 
 app = Flask(__name__)
+
 
 @app.get("/store")
 def get_stores():
@@ -12,10 +14,42 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="Bad request. Ensure 'name' is included in the JSON payload.")
+
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
+
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
+
     return store
+
+
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    # Here not only we need to validate data exists,
+    # But also what type of data. Price should be a float,
+    # for example.
+
+    if ("price" not in item_data or "store_id" not in item_data or "name" not in item_data):
+        abort(400, message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.")
+
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=["Item already exists."])
+
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+
+    return item
 
 
 @app.get("/store/<string:store_id>")
@@ -25,18 +59,7 @@ def get_store(store_id):
 
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
-
-
-@app.post("/item")
-def create_item():
-    item_data = request.get_json()
-    if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}
-    item_id = uuid.uuid4().hex
-    item = {**item_data, "id": item_id}
-    items[item_id] = item
-    return item
+        abort(404, message="Store not found.")
 
 
 @app.get("/item")
@@ -49,4 +72,4 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Item not found"}, 404
+        abort(404, message="Item not found.")
