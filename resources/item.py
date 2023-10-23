f63356281493ba83a69239198a64448a637ae6d5
ducda26 <doananhduc26@gmail.com>
2023-10-23 16:33:00 +0700

    Validation with marshmallow

--- resources/item.py
+++ resources/item.py
@@ -2,6 +2,9 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+
+from schemas import ItemSchemas, ItemUpdateSchema
+
 from db import items
 
 blp = Blueprint("Items", "items", description="Operations on items")
@@ -22,15 +25,8 @@ class Item(MethodView):
         except KeyError:
             abort(404, message="Item not found.")
 
-    def put(self, item_id):
-        item_data = request.get_json()
-        # There's  more validation to do here!
-        # Like making sure price is a number, and also both items are optional
-        # You should also prevent keys that aren't 'price' or 'name' to be passed
-        # Difficult to do with an if statement...
-        if "price" not in item_data or "name" not in item_data:
-            abort(
-                400, message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.")
+    @blp.arguments(ItemUpdateSchema)
+    def put(self, item_data, item_id):
         try:
             item = items[item_id]
             item |= item_data
@@ -44,21 +40,8 @@ class Item(MethodView):
 class ItemList(MethodView):
     def get(self):
         return {'items': list(items.values())}
-
-    def post(self):
-        item_data = request.get_json()
-        # Here not only we need to validate data exists,
-        # But also what type of data. Price should be a float,
-        # for example.
-        if (
-            "price" not in item_data
-            or "store_id" not in item_data
-            or "name" not in item_data
-        ):
-            abort(
-                400,
-                message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
-            )
+    @blp.arguments(ItemSchemas)
+    def post(self, item_data):
         for item in items.values():
             if (
                 item_data["name"] == item["name"]
--- resources/store.py
+++ resources/store.py
@@ -2,6 +2,8 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+
+from schemas import StoreSchema
 from db import stores
 
 blp = Blueprint("stores", __name__, description="Operations on stores")
@@ -29,12 +31,8 @@ class StoreList(MethodView):
     def get(self):
         return {"stores": list(stores.values())}
 
-    def post(self):
-        store_data = request.get_json()
-        if "name" not in store_data:
-            abort(
-                400, message="Bad request. Ensure 'name' is included in the JSON payload.")
-
+    @blp.arguments(StoreSchema)
+    def post(self, store_data):
         for store in stores.values():
             if store_data["name"] == store["name"]:
                 abort(400, message=f"Store already exists.")
