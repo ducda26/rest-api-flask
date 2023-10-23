a38cd5106f7488b8d24ba0187ee21e2ee849247e
ducda26 <doananhduc26@gmail.com>
2023-10-23 18:47:22 +0700

    One-to-many relationships with SQLAlchemy

--- schemas.py
+++ schemas.py
@@ -1,15 +1,27 @@
 from marshmallow import Schema, fields
 
-class ItemSchemas(Schema):
+
+class PlainItemSchemas(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
     store_id = fields.Str(required=True)
-    
+
+
+class PlainStoreSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+
+
+class ItemSchema(PlainItemSchemas):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
-    
-class StoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
+
+
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainItemSchemas()), dump_only=True)
